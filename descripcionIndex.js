635959a77e1db11e33ba6886be0dbd9aaa50e0f4
Juan Martinez Garcia <juanmartinezg2004@gmail.com>
2024-10-08 17:07:26 +0200

    Cambio a interfaz Google

---
+++ descripcionIndex.js
@@ -0,0 +1,14 @@
+const typed = new Typed('.typed', {
+    strings: [
+        'Hola...',
+        'Mi nombre es Juan',
+        'Soy un software developer de 19 años'],
+        typeSpeed: 55,
+        backSpeed: 30,
+        backDelay: 500,
+        startDelay: 1000,
+        loop: false,
+        showCursor: true,
+        cursorChar: '|',
+        contentType: 'html',
+});
